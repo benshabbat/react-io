77b25e334bbad884e27859f854b15acac31f9617
benshabbat <benshabbat27@gmail.com>
2024-09-23 12:54:56 +0300

    handleChange,onSubmit

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,21 +1,42 @@
-import { createElement } from "react";
+import { createElement, useState } from "react";
 
 export default function Form() {
+  const [formData, setFormData] = useState();
+
+  const handleChange = (e) => {
+    const { name, value, checked, type } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+    console.log(value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData)
+  };
   return createElement(
     "form",
-    null,
+    {onSubmit:onSubmit},
     createElement("label", {}, "FirstName"),
     createElement("input", {
       name: "firstName",
       placeholder: "firstName",
+      onChange: handleChange,
     }),
-    createElement("label", {className: "red"}, "LastName"),
+    createElement("label", { className: "red" }, "LastName"),
     createElement("input", {
       name: "lastName",
       placeholder: "lastName",
+      onChange: handleChange,
     }),
     createElement("label", {}, "Age"),
-    createElement("input", { name: "age", placeholder: "age" }),
-    createElement("button", { onClick: () => alert("hello") }, "Submit")
+    createElement("input", {
+      name: "age",
+      placeholder: "age",
+      onChange: handleChange,
+    }),
+    createElement("button", { type:"submit"}, "Submit")
   );
 }
