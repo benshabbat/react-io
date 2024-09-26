389b09330625400d0f54a87caea74ff6225e1efb
benshabbat <benshabbat27@gmail.com>
2024-09-26 09:28:38 +0300

    CardJsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,12 +1,25 @@
 import { createElement } from "react";
 import MyProfile from "../images/my_profile.jpg";
 
-export default function Card() {
+export function Card() {
+  // createElement(type, props, ...children)
+  // type: the element type or component
+  // props: object containing the element's properties (attributes)
+  // children: other elements or text content
+  return createElement(
+    "div",
+    { className: "card" },
+    createElement("img", {className: "imgae", src: MyProfile, alt: "profile card" }, null),
+    createElement("h2", { className: "title" }, "David-Chen Benshabbat"),
+    createElement("p", { className: "description" }, "Learning React IO")
+  );
+}
+export function CardJsx() {
   return (
-    createElement("div",{className:"card"},
-        createElement("img",{src:MyProfile,alt:"profile card"},null),
-        createElement("h2",{},"David-Chen Benshabbat"),
-        createElement("p",{},"Learning React"),
-    )
-  )
+    <div className="card">
+      <img className="image" src={MyProfile} alt="profile Card" />
+      <h2 className="title">David-Chen Benshabbat</h2>
+      <p className="description">Learning React IO</p>
+    </div>
+  );
 }
