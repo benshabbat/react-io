0e1a2487e7aa787a8716d88cc21d568f0e11cf9a
benshabbat <benshabbat27@gmail.com>
2024-10-08 00:01:33 +0300

    starting work on main todo just moved the element and checking from props what i need to send into components and what i moving to comoponent

--- client/src/todoList/MainToDo.jsx
+++ client/src/todoList/MainToDo.jsx
@@ -1,28 +1,44 @@
-import "./todolist.css";
-
 export default function MainToDo() {
 
   return (
-    <section className="main">
-      <input className="toggle-all" type="checkbox" />
-      <ul className="todo-list">
-      <li>
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Test</label>
-          <button className="destroy" />
-        </div>
-        <input className="edit" />
-      </li>
-      <li>
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Test</label>
-          <button className="destroy" />
-        </div>
-        <input className="edit" />
-      </li>
-      </ul>
-    </section>
+<section className="main">
+        <input className="toggle-all" type="checkbox" onClick={toShowOrhide} />
+        <ul className="todo-list">
+          {show &&
+            filteredTodos?.map((todo) => (
+              <li
+                key={todo.id}
+                className={`${todo.completed ? "completed" : ""} ${
+                  editingId === todo.id ? "editing" : ""
+                }`}
+              >
+                <div className="view">
+                  <input
+                    className="toggle"
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={() => toggleTodo(todo.id)}
+                  />
+                  <label onDoubleClick={() => startEditing(todo.id)}>
+                    {todo.title}
+                  </label>
+                  <button
+                    className="destroy"
+                    onClick={() => deleteTodo(todo.id)}
+                  />
+                </div>
+                {editingId === todo.id && (
+                  <input
+                    className="edit"
+                    defaultValue={todo.title}
+                    onBlur={(e) => editTodo(todo.id, e.target.value)}
+                    onKeyDown={(e) => handleEditKeyPress(e, todo.id)}
+                    autoFocus
+                  />
+                )}
+              </li>
+            ))}
+        </ul>
+      </section>
   );
 }
