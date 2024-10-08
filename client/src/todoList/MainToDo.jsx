e0d1fb9cd38beaa4c9a1a673ffeb4d0f2bd64670
benshabbat <benshabbat27@gmail.com>
2024-10-08 11:26:11 +0300

    done edit into main checked

--- client/src/todoList/MainToDo.jsx
+++ client/src/todoList/MainToDo.jsx
@@ -25,7 +25,6 @@ export default function MainToDo(props) {
     return true;
   });
 
-
   const startEditing = (id) => {
     setEditingId(id);
   };
@@ -46,44 +45,42 @@ export default function MainToDo(props) {
     setEditingId(null);
   };
   return (
-<section className="main">
-        <input className="toggle-all" type="checkbox" onClick={toShowOrhide} />
-        <ul className="todo-list">
-          {show &&
-            filteredTodos?.map((todo) => (
-              <li
-                key={todo.id}
-                className={`${todo.completed ? "completed" : ""} ${
-                  editingId === todo.id ? "editing" : ""
-                }`}
-              >
-                <div className="view">
-                  <input
-                    className="toggle"
-                    type="checkbox"
-                    checked={todo.completed}
-                    onChange={() => toggleTodo(todo.id)}
-                  />
-                  <label onDoubleClick={() => startEditing(todo.id)}>
-                    {todo.title}
-                  </label>
-                  <button
-                    className="destroy"
-                    onClick={() => props.deleteTodo(todo.id)}
-                  />
-                </div>
-                {editingId === todo.id && (
-                  <input
-                    className="edit"
-                    defaultValue={todo.title}
-                    onBlur={(e) => editTodo(todo.id, e.target.value)}
-                    onKeyDown={(e) => handleEditKeyPress(e, todo.id)}
-                    autoFocus
-                  />
-                )}
-              </li>
-            ))}
-        </ul>
-      </section>
+    <section className="main">
+      <input className="toggle-all" type="checkbox" onClick={toShowOrhide} />
+      <ul className="todo-list">
+        {show &&
+          filteredTodos?.map((todo) => (
+            <li
+              key={todo.id}
+              className={`${todo.completed ? "completed" : ""} ${
+                editingId === todo.id ? "editing" : ""
+              }`}
+            >
+              <div className="view">
+                <input
+                  className="toggle"
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => toggleTodo(todo.id)}
+                />
+                <label onDoubleClick={() => startEditing(todo.id)}>
+                  {todo.title}
+                </label>
+                <button
+                  className="destroy"
+                  onClick={() => props.deleteTodo(todo.id)}
+                />
+              </div>
+              <input
+                className="edit"
+                defaultValue={todo.title}
+                onBlur={(e) => editTodo(todo.id, e.target.value)}
+                onKeyDown={(e) => handleEditKeyPress(e, todo.id)}
+                autoFocus
+              />
+            </li>
+          ))}
+      </ul>
+    </section>
   );
 }
